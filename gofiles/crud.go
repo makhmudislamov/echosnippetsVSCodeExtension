29b5c752f4013baa77532ea48da5935ed06f8bd7
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-03-02 14:51:41 -0800

    added boyd of crud snippet

---
+++ gofiles/crud.go
@@ -0,0 +1,25 @@
+package packageName
+
+import (
+	"net/http"	
+	"github.com/labstack/echo"
+)
+
+// Handlers
+func create(c echo.Context) error {
+}
+
+func find(c echo.Context) error {
+}
+
+func update(c echo.Context) error {
+}
+
+func delete(c echo.Context) error {
+}
+
+// Routes
+e.POST("/createRoute", create)
+e.GET("/findRoute", find)
+e.PUT("/updateRoute", update)
+e.DELETE("/deleteRoute", delete)
--- gofiles/hello.go
+++ gofiles/hello.go
@@ -2,7 +2,7 @@ package packageName
 
 import (
 	"net/http"
-	
+	"fmt"
 	"github.com/labstack/echo"
 )
 
@@ -10,6 +10,7 @@ func main() {
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
+		fmt.Println("rendering Hello World")
 	})
 	e.Logger.Fatal(e.Start(":1323"))
 }
