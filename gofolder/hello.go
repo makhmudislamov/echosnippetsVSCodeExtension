2cffe2b1861df524a4b2ee6c10e83d04b7e1a12d
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-03-05 15:32:07 -0800

    big fix

--- gofolder/hello.go
+++ gofolder/hello.go
@@ -2,7 +2,6 @@ package packagename
 
 import (
 	"net/http"
-
 	"github.com/labstack/echo"
 )
 
@@ -10,7 +9,7 @@ func main() {
 	e := echo.New()
 
 	e.GET("/", func(c echo.Context) error {
-			return c.String(http.StatusOK, "Hello, World!")
+		return c.String(http.StatusOK, "Hello, World!")
 	})
 
 	e.Logger.Fatal(e.Start(":3000"))
