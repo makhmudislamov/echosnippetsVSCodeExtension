78077010cd34e28ea48144c42b8e8c27c87dc2e1
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-03-05 15:02:25 -0800

    reformatted

--- gofolder/hello.go
+++ gofolder/hello.go
@@ -1,16 +1,17 @@
 package packagename
 
 import (
-        "github.com/labstack/echo"
-        "net/http"
+	"net/http"
+
+	"github.com/labstack/echo"
 )
 
 func main() {
-        e := echo.New()
+	e := echo.New()
 
-        e.GET("/", func(c echo.Context) error {
-                return c.String(http.StatusOK, "Hello, World!")
-        })
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
 
-        e.Logger.Fatal(e.Start(":3000"))
+	e.Logger.Fatal(e.Start(":3000"))
 }
