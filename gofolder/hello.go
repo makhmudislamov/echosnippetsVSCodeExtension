36442b515ac266c9fe9ce32a6a0a5343fb11f707
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-03-05 14:33:26 -0800

    reformatting

--- gofolder/hello.go
+++ gofolder/hello.go
@@ -7,12 +7,12 @@ import (
 )
 
 func main() {
-	e := echo.New()
+		e := echo.New()
 
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
-		fmt.Println("rendering Hello World")
-	})
-	
-	e.Logger.Fatal(e.Start(":3000"))
+		e.GET("/", func(c echo.Context) error {
+				return c.String(http.StatusOK, "Hello, World!")
+				fmt.Println("rendering Hello World")
+		})
+		
+		e.Logger.Fatal(e.Start(":3000"))
 }
