a5f111bd07a4f2ddc7f39c97148d1bbf1466153a
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-03-23 11:57:42 -0700

    test snippet setup is ready

--- gofolder/test.go
+++ gofolder/test.go
@@ -10,12 +10,14 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+// TestFuncName1 is the name of the function that is tested
 func TestFuncName1(t *testing.T) {
 	// Setup
 
 	// Assertions
 }
 
+// TestFuncName2 is the name of the function that is tested
 func TestFuncName2(t *testing.T) {
 	// Setup
 
