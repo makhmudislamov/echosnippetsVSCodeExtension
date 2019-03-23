d8fccf2bc7895a38997a86b958a98e40b8be4aa0
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-03-23 13:17:27 -0700

    auth final

--- gofolder/auth.go
+++ gofolder/auth.go
@@ -1,79 +1,15 @@
-
-package middleware
+package packagename
 
 import (
-	"encoding/base64"
-	"strconv"
-	"strings"
-
-	"github.com/labstack/echo/v4"
+	"fmt"
+	"github.com/labstack/echo"
+	"github.com/spf13/viper"
+	"net/http"
 )
 
-type (
-	// BasicAuthConfig defines the config for BasicAuth middleware.
-	BasicAuthConfig struct {
-		// Skipper defines a function to skip middleware.
-		Skipper Skipper
-
-		// Validator is a function to validate BasicAuth credentials.
-		// Required.
-		Validator BasicAuthValidator
-
-		// Realm is a string to define realm attribute of BasicAuth.
-		// Default value "Restricted".
-		Realm string
+func ValidateUser(username, password string, c echo.Context) (bool, error) {
+	if username == "joe" && password == "secret" {
+		return true, nil
 	}
-
-	// BasicAuthValidator defines a function to validate BasicAuth credentials.
-	BasicAuthValidator func(string, string, echo.Context) (bool, error)
-)
-
-const (
-	basic        = "basic"
-	defaultRealm = "Restricted"
-)
-
-var (
-	// DefaultBasicAuthConfig is the default BasicAuth middleware config.
-	DefaultBasicAuthConfig = BasicAuthConfig{
-		Skipper: DefaultSkipper,
-		Realm:   defaultRealm,
-	}
-)
-
-// BasicAuth returns an BasicAuth middleware.
-//
-// For valid credentials it calls the next handler.
-// For missing or invalid credentials, it sends "401 - Unauthorized" response.
-func BasicAuth(fn BasicAuthValidator) echo.MiddlewareFunc {
-	c := DefaultBasicAuthConfig
-	c.Validator = fn
-	return BasicAuthWithConfig(c)
-}
-
-// BasicAuthWithConfig returns an BasicAuth middleware with config.
-// See `BasicAuth()`.
-func BasicAuthWithConfig(config BasicAuthConfig) echo.MiddlewareFunc {
-	// Defaults
-	if config.Validator == nil {
-		panic("echo: basic-auth middleware requires a validator function")
-	}
-	if config.Skipper == nil {
-		config.Skipper = DefaultBasicAuthConfig.Skipper
-	}
-	if config.Realm == "" {
-		config.Realm = defaultRealm
-	}
-
-	return func(next echo.HandlerFunc) echo.HandlerFunc {
-		return func(c echo.Context) error {
-	
-			}
-
-			// Authtentication and credential verification code goes here
-
-			// Need to return `401` for browsers to pop-up login box.
-			
-		}
-	}
+	return false, nil
 }
