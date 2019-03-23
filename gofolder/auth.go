98ac8b45b531535f82126c3ac16ca0192f9be079
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-03-23 13:14:14 -0700

    auth setup is done

--- gofolder/auth.go
+++ gofolder/auth.go
@@ -1,4 +1,5 @@
-package packagename
+
+package middleware
 
 import (
 	"encoding/base64"
@@ -8,32 +9,71 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
+type (
+	// BasicAuthConfig defines the config for BasicAuth middleware.
+	BasicAuthConfig struct {
+		// Skipper defines a function to skip middleware.
+		Skipper Skipper
 
-BasicAuthConfig struct {
-  // Skipper defines a function to skip middleware.
-  Skipper Skipper
+		// Validator is a function to validate BasicAuth credentials.
+		// Required.
+		Validator BasicAuthValidator
 
-  // Validator is a function to validate BasicAuth credentials.
-  // Required.
-  Validator BasicAuthValidator
+		// Realm is a string to define realm attribute of BasicAuth.
+		// Default value "Restricted".
+		Realm string
+	}
 
-  // Realm is a string to define realm attribute of BasicAuth.
-  // Default value "Restricted".
-  Realm string
+	// BasicAuthValidator defines a function to validate BasicAuth credentials.
+	BasicAuthValidator func(string, string, echo.Context) (bool, error)
+)
+
+const (
+	basic        = "basic"
+	defaultRealm = "Restricted"
+)
+
+var (
+	// DefaultBasicAuthConfig is the default BasicAuth middleware config.
+	DefaultBasicAuthConfig = BasicAuthConfig{
+		Skipper: DefaultSkipper,
+		Realm:   defaultRealm,
+	}
+)
+
+// BasicAuth returns an BasicAuth middleware.
+//
+// For valid credentials it calls the next handler.
+// For missing or invalid credentials, it sends "401 - Unauthorized" response.
+func BasicAuth(fn BasicAuthValidator) echo.MiddlewareFunc {
+	c := DefaultBasicAuthConfig
+	c.Validator = fn
+	return BasicAuthWithConfig(c)
 }
 
-
-e.Use(middleware.BasicAuth(func(username, password string, c echo.Context) (bool, error) {
-	if username == "joe" && password == "secret" {
-		return true, nil
+// BasicAuthWithConfig returns an BasicAuth middleware with config.
+// See `BasicAuth()`.
+func BasicAuthWithConfig(config BasicAuthConfig) echo.MiddlewareFunc {
+	// Defaults
+	if config.Validator == nil {
+		panic("echo: basic-auth middleware requires a validator function")
 	}
-	return false, nil
-}))
+	if config.Skipper == nil {
+		config.Skipper = DefaultBasicAuthConfig.Skipper
+	}
+	if config.Realm == "" {
+		config.Realm = defaultRealm
+	}
 
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+	
+			}
 
-e.Use(middleware.BasicAuthWithConfig(middleware.BasicAuthConfig{}))
+			// Authtentication and credential verification code goes here
 
-
-DefaultBasicAuthConfig = BasicAuthConfig{
-	Skipper: DefaultSkipper,
+			// Need to return `401` for browsers to pop-up login box.
+			
+		}
+	}
 }
